77106d9378b92b1daec5a07b364ca4b4e16cb792
Majid <majid.asadollahi95@gmail.com>
2024-06-06 23:46:32 +0330

    finish date layer

---
+++ DataLayer/Services/PageGroupRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class PageGroupRepository : IPageGroupRepository
+    {
+        private MyCmsContext db = new MyCmsContext();
+        public IEnumerable<PageGroup> GetAllGroups()
+        {
+            return db.PageGroups;
+        }
+
+        public PageGroup GetGroupById(int groupId)
+        {
+            return db.PageGroups.Find(groupId);
+        }
+
+        public bool InsertGroup(PageGroup pageGroup)
+        {
+            try
+            {
+                db.PageGroups.Add(pageGroup);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateGroup(PageGroup pageGroup)
+        {
+            try
+            {
+                db.Entry(pageGroup).State = EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteGroup(PageGroup pageGroup)
+        {
+            try
+            {
+                db.Entry(pageGroup).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteGroup(int groupId)
+        {
+            try
+            {
+                var group = GetGroupById(groupId);
+                DeleteGroup(groupId);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+
+    }
+}
